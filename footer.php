33135cbcf78d0458716c0a899444c733e846504f
magn3054 <mdch20@student.aau.dk>
2024-09-29 15:24:21 +0200

    php test

---
+++ footer.php
@@ -0,0 +1,8 @@
+<footer>
+    <div id="f-logo"></div>
+    <div id="f-container">
+        <div class="f-text f-left">hej</div>
+        <div class="f-text f-mid">hej</div>
+        <div class="f-text f-right">hej</div>
+    </div>
+</footer>
---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="da">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nippon - Asiatiske Måltidskasser</title>
+    <script defer src="js/index.js"></script>
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+    <header>
+        <!-- <picture>
+            <source srcset="img/top_image.webp" type="image/webp">
+            <img src="img/top_image.png" alt="Nippon - Asiatiske Måltidskasser">
+        </picture> -->
+    </header>
+    <section></section>
+    <section></section>
+    <section>
+        <article></article>
+        <article></article>
+        <article></article>
+    </section>
+    <?php include 'footer.php'; ?>
+</body>
+</html>
