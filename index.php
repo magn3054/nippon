7da7e750863d1a4021bfef8e15e6b78968335109
magn3054 <mdch20@student.aau.dk>
2024-10-09 16:51:03 +0200

    javascript linekd correctly

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nippon - Asiatiske Måltidskasser</title>
-    <script defer src="js/index.js"></script>
+    <script defer src="js/links.js"></script>
     <link rel="stylesheet" href="css/header.css">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/footer.css">
