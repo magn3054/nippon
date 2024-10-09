563bf4f12dc969dd56227932ab775b048691d967
magn3054 <mdch20@student.aau.dk>
2024-10-09 16:57:38 +0200

    fe3

--- error-page.php
+++ error-page.php
@@ -8,17 +8,13 @@
     <script defer src="js/links.js"></script>
     <link rel="icon" href="img/favicon-logo.svg" type="image/x-icon">
     <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/header.css">
-    <link rel="stylesheet" href="css/footer.css">
     <link rel="stylesheet" href="css/error-page.css">
 </head>
 <body>
-    <?php include 'header.php'; ?>
     <main>
         <h1>Hovsa</h1>
         <p>Denne side findes ikke, så det var bare ærgeligt</p>
         <img onclick="indexPage()" id="error-image" src="img/stiv-fisk.webp" alt="">
     </main>
-    <?php include 'footer.php'; ?>
 </body>
 </html>
