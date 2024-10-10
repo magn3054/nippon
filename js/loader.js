9f96aaa332ae8eaf0ae52cec6b07752216b2e286
leaxnimb <eaa23lnlu@students.eaaa.dk>
2024-10-10 11:46:18 +0200

    byt ud knap
    tadaaa

--- js/loader.js
+++ js/loader.js
@@ -1,2 +1,11 @@
 "use strict";
 
+function byt() {
+    const dinKasse = document.getElementById('dit-billede');
+    const andenKasse = document.getElementById('andet-billede');
+    const temp = dinKasse.src
+
+     dinKasse.src = andenKasse.src;
+     andenKasse.src = temp;
+}
+
